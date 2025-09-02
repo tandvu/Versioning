6476f235b64ca266be4d518bf4f4b6df39c33c5a
Tan Vu <tan.d.vu.civ@us.navy.mil>
2025-09-02 10:15:22 -0700

    chore: update client ESLint config and package files

--- repo-browser/client/.eslintrc.cjs
+++ repo-browser/client/.eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
     sourceType: 'module',
     ecmaFeatures: { jsx: true }
   },
-  plugins: ['@typescript-eslint','react','react-hooks'],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -17,13 +17,13 @@ module.exports = {
   env: { browser: true, es2021: true },
   rules: {
     'react/react-in-jsx-scope': 'off',
-  '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-  '@typescript-eslint/explicit-module-boundary-types': 'off',
-  // Allow explicit any in legacy code and small utilities to reduce churn
-  '@typescript-eslint/no-explicit-any': 'off',
-  // This is a TSX project; prop-types are unnecessary when using TypeScript types
-  'react/prop-types': 'off',
-  // Regex contains control characters (ANSI color escapes) used for log stripping
-  'no-control-regex': 'off'
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // Allow explicit any in legacy code and small utilities to reduce churn
+    '@typescript-eslint/no-explicit-any': 'off',
+    // This is a TSX project; prop-types are unnecessary when using TypeScript types
+    'react/prop-types': 'off',
+    // Regex contains control characters (ANSI color escapes) used for log stripping
+    'no-control-regex': 'off'
   }
 };
